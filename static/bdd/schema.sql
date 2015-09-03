1df1b6c8535d755800111d2c5f355945ff8c08b7
begarco <benoit.garcon@outlook.com>
2015-09-03 22:18:47 +0200

    suppression de fichiers inutiles

--- static/bdd/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists entries;
-create table articles (
-  id integer primary key autoincrement,
-  title text not null,
-  text text not null,
-  date date not null,
-  auth text not null
-);
